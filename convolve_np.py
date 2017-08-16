751d4288d675292e5e36ecdf679fd55efb93f410
Stefan Stavrev <machinelearninggod1988@gmail.com>
2017-08-16 13:07:12 +0200

    sobel edge operator

---
+++ convolve_np.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# X and F are numpy matrices
+def convolve_np(X, F):
+    X_height = X.shape[0]
+    X_width = X.shape[1]
+
+    F_height = F.shape[0]
+    F_width = F.shape[1]
+    
+    H = (F_height - 1) / 2
+    W = (F_width - 1) / 2
+    
+    out = np.zeros((X_height, X_width))
+    
+    for i in np.arange(H, X_height-H):
+        for j in np.arange(W, X_width-W):
+            sum = 0
+            for k in np.arange(-H, H+1):
+                for l in np.arange(-W, W+1):
+                    a = X[i+k, j+l]
+                    w = F[H+k, W+l]
+                    sum += (w * a)
+            out[i,j] = sum
+        
+    return out
